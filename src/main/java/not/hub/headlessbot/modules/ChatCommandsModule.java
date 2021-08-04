022fd66c346d41df63f6834151c4ba18728dcc91
nothub <48992448+nothub@users.noreply.github.com>
2021-08-04 13:53:33 +0200

    command parser

--- src/main/java/not/hub/headlessbot/modules/ChatCommandsModule.java
+++ src/main/java/not/hub/headlessbot/modules/ChatCommandsModule.java
@@ -1,11 +1,20 @@
 package not.hub.headlessbot.modules;
 
+import net.minecraft.network.play.client.CPacketChatMessage;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import not.hub.headlessbot.Log;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ChatCommandsModule extends Module {
+
+    public static final String PREFIX = "~";
+
+    Pattern com = Pattern.compile("^(<[a-zA-Z0-9_]{3,16}>)\\s(.+)$");
+    Pattern tpa = Pattern.compile("^([a-zA-Z0-9_]{3,16}) wants to teleport to you\\.$");
 
     public ChatCommandsModule() {
         super(Type.ALWAYS_ACTIVE);
@@ -13,7 +22,27 @@ public class ChatCommandsModule extends Module {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onChat(ClientChatReceivedEvent ev) {
-        Log.info(getClass(), ev.getMessage().getUnformattedText());
+        // Important! We ignore the difference of chat and system type messages,
+        // because half of the server have fucked the logic with plugins anyways...
+        Matcher tpaMatcher = tpa.matcher(ev.getMessage().getUnformattedText());
+        if (tpaMatcher.find()) {
+            final String requester = tpaMatcher.group(1);
+            if (requester != null && !requester.isEmpty()) {
+                mc.addScheduledTask(() -> mc.player.connection.sendPacket(
+                    new CPacketChatMessage("/tpy " + requester)));
+                Log.info(name, "Accepting tpa from " + requester);
+                return;
+            }
+        }
+        Matcher commandMatcher = com.matcher(ev.getMessage().getUnformattedText());
+        if (commandMatcher.find()) {
+            final String sender = commandMatcher.group(1);
+            final String message = commandMatcher.group(2);
+            if (sender != null && !sender.isEmpty() && message != null && !message.isEmpty() && message.startsWith(PREFIX)) {
+                final String command = message.replaceFirst(PREFIX, "");
+                Log.info(name, sender + " sent command: " + command);
+            }
+        }
     }
 
 }
