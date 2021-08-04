f5b88072314bf14d1595fd5d3da915315d36e2af
nothub <48992448+nothub@users.noreply.github.com>
2021-08-04 17:11:37 +0200

    suicide on stuck

--- src/main/java/not/hub/headlessbot/modules/StuckDetectorModule.java
+++ src/main/java/not/hub/headlessbot/modules/StuckDetectorModule.java
@@ -1,6 +1,7 @@
 package not.hub.headlessbot.modules;
 
 import cc.neckbeard.utils.ExpiringFlag;
+import net.minecraft.network.play.client.CPacketChatMessage;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import not.hub.headlessbot.Log;
@@ -8,8 +9,8 @@ import not.hub.headlessbot.Log;
 import java.time.temporal.ChronoUnit;
 
 public class StuckDetectorModule extends Module {
-    private static final ExpiringFlag cooldown = new ExpiringFlag(1, ChronoUnit.MINUTES, true);
-    private static long lastX;
+    private static final ExpiringFlag cooldown = new ExpiringFlag(15, ChronoUnit.MINUTES, true);
+    private static int lastX;
     private static int lastZ;
 
     public StuckDetectorModule() {
@@ -24,7 +25,9 @@ public class StuckDetectorModule extends Module {
         if (cooldown.isValid()) return;
         else cooldown.reset();
         if (mc.player.chunkCoordX == lastX && mc.player.chunkCoordZ == lastZ) {
-            Log.warn(name, "Im stuck in the same chunk (" + lastX + "x " + lastZ + "z) as last minute!");
+            Log.warn(name, "Im stuck in the same chunk (" + lastX + "x " + lastZ + "z) as 15 minutes ago, imma get out of here...");
+            mc.player.connection.sendPacket(new CPacketChatMessage("/kill"));
+            return;
         }
         lastX = mc.player.chunkCoordX;
         lastZ = mc.player.chunkCoordZ;
