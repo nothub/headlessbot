839da58ecc9370c0f82cd0a700ad4891be486f4b
nothub <48992448+nothub@users.noreply.github.com>
2021-08-05 18:51:30 +0200

    format util

---
+++ src/main/java/not/hub/headlessbot/StringFormat.java
@@ -0,0 +1,23 @@
+package not.hub.headlessbot;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
+
+public class StringFormat {
+
+    public static String of(EntityPlayer player) {
+        return of(player.getPosition()) + " " + of(player.dimension);
+    }
+
+    public static String of(BlockPos position) {
+        return position.getX() + "x " + position.getY() + "y " + position.getZ() + "z";
+    }
+
+    public static String of(int dim) {
+        if (dim == 0) return "OVERWORLD";
+        else if (dim == 1) return "NETHER";
+        else if (dim == -1) return "END";
+        else return "CUSTOM (" + dim + ")";
+    }
+
+}
