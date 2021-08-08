01709e6b5f53ff42b68cdfb2d41e65631f76b03c
nothub <48992448+nothub@users.noreply.github.com>
2021-08-08 23:53:02 +0200

    comment

--- src/main/java/not/hub/headlessbot/modules/DisconnectDetectorModule.java
+++ src/main/java/not/hub/headlessbot/modules/DisconnectDetectorModule.java
@@ -4,18 +4,21 @@ import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import not.hub.headlessbot.Bot;
 import not.hub.headlessbot.Log;
-import not.hub.headlessbot.fsm.Startup;
+import not.hub.headlessbot.fsm.StartupFsm;
 
-// TODO: why is this a module?
+// TODO: why is this a module? we also need this logic while in queue...
 public class DisconnectDetectorModule extends Module {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onInitGui(GuiScreenEvent.InitGuiEvent.Post event) {
         if (!(event.getGui() instanceof GuiDisconnected)) return;
         Log.info(getClass(), "Server connection lost...");
-        if (Startup.getCurrent() == Startup.State.ACTIVE || Startup.getCurrent() == Startup.State.QUEUE) Startup.transition(false);
-        else throw new IllegalStateException("Invalid fsm transition source state" + Startup.getCurrent().name());
+        if (Bot.FSM_STARTUP.current() == StartupFsm.State.ACTIVE
+            || Bot.FSM_STARTUP.current() == StartupFsm.State.QUEUE)
+            Bot.FSM_STARTUP.transition(false);
+        else throw new IllegalStateException("Invalid fsm transition source state" + Bot.FSM_STARTUP.current().name());
     }
 
 }
