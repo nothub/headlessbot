8a22fc1a23af5d6333fe703040b328702c2bb660
nothub <48992448+nothub@users.noreply.github.com>
2021-10-02 18:40:58 +0200

    simplify dt

--- src/main/java/not/hub/headlessbot/fsm/DtNode.java
+++ src/main/java/not/hub/headlessbot/fsm/DtNode.java
@@ -1,13 +1,50 @@
 package not.hub.headlessbot.fsm;
 
-public abstract class DtNode {
+import java.util.function.Supplier;
 
-    public final String name;
+public class DtNode {
 
-    public DtNode(String name) {
-        this.name = name;
+    private final Runnable r;
+    private final Supplier<Boolean> s;
+    private DtNode y;
+    private DtNode n;
+
+    public DtNode(Runnable r) {
+        this.r = r;
+        this.s = null;
     }
 
-    public abstract Boolean isLeaf();
+    public DtNode(Supplier<Boolean> s) {
+        this.r = null;
+        this.s = s;
+    }
+
+    public DtNode(Supplier<Boolean> s, DtNode y, DtNode n) {
+        this(s);
+        this.y = y;
+        this.n = n;
+    }
+
+    public void setY(DtNode y) {
+        this.y = y;
+    }
+
+    public void setN(DtNode n) {
+        this.n = n;
+    }
+
+    public DtNode next() {
+        if (r != null) r.run();
+        else if (s == null || y == null || n == null) throw new IllegalStateException("Invalid Node");
+        else return s.get() ? y : n;
+        return null;
+    }
+
+    public void walk() {
+        DtNode n = this;
+        while (n != null) {
+            n = n.next();
+        }
+    }
 
 }
