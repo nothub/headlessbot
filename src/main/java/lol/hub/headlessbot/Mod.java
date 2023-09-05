9bdb1d52f82764c38d19fbf718f54a0e9bae09ce
Florian Hübner <code@hub.lol>
2023-09-06 00:58:38 +0200

    auto respawn

--- src/main/java/lol/hub/headlessbot/Mod.java
+++ src/main/java/lol/hub/headlessbot/Mod.java
@@ -2,10 +2,8 @@ package lol.hub.headlessbot;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.text.Text;
 
 public class Mod implements ModInitializer, ClientModInitializer {
 
@@ -19,15 +17,25 @@ public class Mod implements ModInitializer, ClientModInitializer {
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null) return;
-            // onTick()
+
+            // respawn
+            if (client.player.isDead()) client.player.requestRespawn();
         });
 
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            System.err.println("client connected");
+            var netHandler = client.getNetworkHandler();
+            if (netHandler == null) return;
+
+            var serverInfo = netHandler.getServerInfo();
+            if (serverInfo == null) return;
+
+            System.err.printf("client connected to %s (%s)\n", serverInfo.name, serverInfo.address);
         });
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
             System.err.println("client disconnected");
+            client.close();
+            //System.exit(0);
         });
     }
 }
