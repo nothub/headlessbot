cf50e0039cc3d45ab69611a9ddb8f2fa2b91ffbb
Florian Hübner <code@hub.lol>
2023-09-06 01:26:35 +0200

    detect players

--- src/main/java/lol/hub/headlessbot/Mod.java
+++ src/main/java/lol/hub/headlessbot/Mod.java
@@ -4,10 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 
 public class Mod implements ModInitializer, ClientModInitializer {
 
     public static long lastKeepAlive;
+    public static long ticksOnline;
 
     @Override
     public void onInitialize() {
@@ -16,10 +18,20 @@ public class Mod implements ModInitializer, ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null) return;
+            if (client.player == null || client.world == null) return;
+            ticksOnline++;
 
-            // respawn
-            if (client.player.isDead()) client.player.requestRespawn();
+            if (client.player.isDead()) {
+                if (ticksOnline % 20 == 0) {
+                    System.err.println("requesting respawn");
+                    client.player.requestRespawn();
+                }
+                return;
+            }
+
+            for (AbstractClientPlayerEntity player : client.world.getPlayers()) {
+                System.err.printf("seeing player: %s (%s)\n", player.getGameProfile().getName(), player.getUuid().toString());
+            }
         });
 
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
