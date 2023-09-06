69deaee4f59e33ffa323f7011af87a0b95bfc20b
Florian Hübner <code@hub.lol>
2023-09-06 14:34:00 +0200

    archive name

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ mkdir -p run
 cd run
 
 # shellcheck disable=SC2139
-alias headlessmc="docker run --rm -v ${PWD}/mc:/work/.minecraft -v ${PWD}/hmc:/work/HeadlessMC n0thub/headlessmc:latest"
+alias headlessmc="docker run -it --rm -v ${PWD}/mc:/work/.minecraft -v ${PWD}/hmc:/work/HeadlessMC n0thub/headlessmc:latest"
 
 # msa login
 if test ! -f "hmc/auth/.account.json"; then
@@ -32,16 +32,22 @@ mkdir -p mc/mods
 
 # install fabric api
 if test ! -e "mc/mods/fabric-api-0.87.0+1.19.4.jar"; then
-    curl -sSLO "https://cdn.modrinth.com/data/P7dR8mSH/versions/LKgVmlZB/fabric-api-0.87.0+1.19.4.jar"
+    (
+        cd mc/mods
+        curl -sSLO "https://cdn.modrinth.com/data/P7dR8mSH/versions/LKgVmlZB/fabric-api-0.87.0+1.19.4.jar"
+    )
 fi
 
 # install baritone
 if test ! -e "mc/mods/baritone-api-fabric-1.9.3.jar"; then
-    curl -sSLO "https://github.com/cabaletta/baritone/releases/download/v1.9.3/baritone-api-fabric-1.9.3.jar"
+    (
+        cd mc/mods
+        curl -sSLO "https://github.com/cabaletta/baritone/releases/download/v1.9.3/baritone-api-fabric-1.9.3.jar"
+    )
 fi
 
 # install mod
-cp "../build/libs/bot.jar" "mc/mods/"
+cp ../build/libs/*.jar mc/mods/
 
 # run client
 headlessmc launch "fabric-loader-0.14.22-1.19.4"
