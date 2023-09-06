94a09515c3427d6c35237ed0c0f41d009ffe97f2
Florian Hübner <code@hub.lol>
2023-09-06 20:24:05 +0200

    todos

--- src/main/java/lol/hub/headlessbot/behaviour/BehaviourTree.java
+++ src/main/java/lol/hub/headlessbot/behaviour/BehaviourTree.java
@@ -3,6 +3,7 @@ package lol.hub.headlessbot.behaviour;
 public class BehaviourTree {
 
     /*
+    TODO: tree walking and node types
     https://www.gamedeveloper.com/programming/behavior-trees-for-ai-how-they-work
     */
 
