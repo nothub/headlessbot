f412b568b4209ef72d9b5cb8c8ed498b58a84bf5
Florian Hübner <code@hub.lol>
2023-09-06 03:26:36 +0200

    bus

--- src/main/java/lol/hub/headlessbot/Mod.java
+++ src/main/java/lol/hub/headlessbot/Mod.java
@@ -1,12 +1,18 @@
 package lol.hub.headlessbot;
 
+import baritone.api.BaritoneAPI;
+import lol.hub.tinyeventbus.Bus;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 
+import java.util.stream.Collectors;
+
 public class Mod implements ModInitializer, ClientModInitializer {
+
+    public static final Bus bus = new Bus();
 
     public static long lastKeepAlive;
     public static long ticksOnline;
@@ -17,6 +23,10 @@ public class Mod implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+
+        });
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player == null || client.world == null) return;
             ticksOnline++;
@@ -43,6 +53,8 @@ public class Mod implements ModInitializer, ClientModInitializer {
             if (serverInfo == null) return;
 
             Log.info("client connected to %s (%s)", serverInfo.name, serverInfo.address);
+
+            configureBaritone();
         });
 
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
@@ -50,5 +62,109 @@ public class Mod implements ModInitializer, ClientModInitializer {
             client.close();
             //System.exit(0);
         });
+    }
+
+    private void configureBaritone() {
+        var settings = BaritoneAPI.getSettings();
+
+        settings.allowBreak.value = true;
+        settings.allowPlace.value = true;
+        settings.allowInventory.value = true;
+        settings.allowJumpAt256.value = true;
+        settings.allowWaterBucketFall.value = true;
+        settings.maxFallHeightBucket.value = 256;
+
+        settings.allowParkour.value = true;
+        settings.allowParkourPlace.value = true;
+        settings.allowParkourAscend.value = true;
+        settings.allowDiagonalDescend.value = true;
+        settings.allowDiagonalAscend.value = true;
+
+        settings.exploreForBlocks.value = true;
+
+        // assume no jesus as default
+        settings.assumeWalkOnWater.value = false;
+        settings.assumeWalkOnLava.value = false;
+
+        settings.allowDownward.value = true;
+        settings.allowVines.value = true;
+        settings.allowWalkOnBottomSlab.value = true;
+        settings.enterPortal.value = true;
+
+        settings.considerPotionEffects.value = true;
+        settings.rightClickContainerOnArrival.value = true;
+
+        settings.replantCrops.value = true;
+        settings.replantNetherWart.value = true;
+
+        settings.mineScanDroppedItems.value = true;
+        settings.legitMine.value = false;
+
+        settings.pauseMiningForFallingBlocks.value = true;
+        settings.avoidUpdatingFallingBlocks.value = true;
+
+        settings.buildIgnoreExisting.value = true;
+        settings.okIfWater.value = true;
+
+        settings.avoidance.value = true;
+        settings.mobSpawnerAvoidanceRadius.value = 16;
+        settings.mobAvoidanceRadius.value = 16;
+
+        settings.followOffsetDistance.value = 4d;
+        settings.followRadius.value = 8;
+
+        settings.echoCommands.value = true;
+        settings.prefixControl.value = true;
+        settings.chatControl.value = false;
+        settings.chatControlAnyway.value = false;
+        settings.chatDebug.value = false;
+
+        // we dont see this stuff anyways
+        settings.renderPath.value = false;
+        settings.renderPathAsLine.value = false;
+        settings.renderGoal.value = false;
+        settings.renderSelectionBoxes.value = false;
+        settings.renderGoalXZBeacon.value = false;
+        settings.renderCachedChunks.value = false;
+        settings.renderSelection.value = false;
+        settings.renderSelectionCorners.value = false;
+        settings.desktopNotifications.value = false;
+
+        //settings.acceptableThrowawayItems.value.addAll(BlockGroups.PATHING_BLOCKS.items);
+
+        Log.info("Baritone initialized with settings:\n"
+            + settings
+            .allSettings
+            .stream()
+            .map(s -> "  - " + s.getName() + ": " + s.value)
+            .collect(Collectors.joining("\n"))
+        );
+
+        /*
+         *       TODO: check hunger levels
+         *     public final Settings$Setting<Boolean> allowSprint;
+         *     public final Settings$Setting<Boolean> sprintInWater;
+         *
+         *       TODO: sync from module states
+         *     public final Settings$Setting<Integer> maxFallHeightNoWater;
+         *     public final Settings$Setting<Boolean> antiCheatCompatibility;
+         *     public final Settings$Setting<Boolean> assumeExternalAutoTool;
+         *     public final Settings$Setting<Boolean> assumeWalkOnWater;
+         *     public final Settings$Setting<Boolean> assumeWalkOnLava;
+         *     public final Settings$Setting<Boolean> assumeStep;
+         *     public final Settings$Setting<Boolean> assumeSafeWalk;
+         *
+         *       TODO: builder mode
+         *     public final Settings$Setting<Boolean> buildInLayers;
+         *     public final Settings$Setting<Boolean> layerOrder;
+         *     public final Settings$Setting<Integer> startAtLayer;
+         *     public final Settings$Setting<Boolean> skipFailedLayers;
+         *     public final Settings$Setting<Boolean> mapArtMode;
+         *     public final Settings$Setting<Boolean> schematicOrientationX;
+         *     public final Settings$Setting<Boolean> schematicOrientationY;
+         *     public final Settings$Setting<Boolean> schematicOrientationZ;
+         *
+         */
+
     }
 }
