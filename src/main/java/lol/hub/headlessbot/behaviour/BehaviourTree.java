8ffb2aac91edd72fdbdbde16df27a07f84517c79
Florian Hübner <code@hub.lol>
2023-09-11 01:06:59 +0200

    tree validation

--- src/main/java/lol/hub/headlessbot/behaviour/BehaviourTree.java
+++ src/main/java/lol/hub/headlessbot/behaviour/BehaviourTree.java
@@ -1,6 +1,9 @@
 package lol.hub.headlessbot.behaviour;
 
 import lol.hub.headlessbot.behaviour.nodes.Node;
+import lol.hub.headlessbot.behaviour.nodes.composites.CompositeNode;
+import lol.hub.headlessbot.behaviour.nodes.decorators.DecoratorNode;
+import lol.hub.headlessbot.behaviour.nodes.leafs.LeafNode;
 
 public class BehaviourTree implements Tickable {
     /* https://www.gamedeveloper.com/programming/behavior-trees-for-ai-how-they-work */
@@ -15,7 +18,37 @@ public class BehaviourTree implements Tickable {
         return root;
     }
 
+    @Override
     public State tick() {
         return root.tick();
+    }
+
+    public void validate() {
+        if (root == null) {
+            throw new AssertionError("tree has no root node");
+        }
+        validate(root);
+    }
+
+    public void validate(Node node) {
+        if (node instanceof CompositeNode n) {
+            if (n.children().size() < 2) {
+                throw new AssertionError("composite node with less then 2 children");
+            }
+            for (Node c : n.children()) {
+                if (c == null) {
+                    throw new AssertionError("composite node has null child");
+                }
+                validate(c);
+            }
+        } else if (node instanceof DecoratorNode n) {
+            if (n.child() == null) {
+                throw new AssertionError("decorator node without child");
+            }
+            validate(n.child());
+        } else if (node instanceof LeafNode n) {
+
+
+        }
     }
 }
