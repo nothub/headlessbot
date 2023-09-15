72af8edb9aea2745b9a059c2cb6912728ad7799e
Florian Hübner <code@hub.lol>
2023-09-16 00:45:51 +0200

    fix

--- src/main/java/lol/hub/headlessbot/behavior/nodes/composites/FallbackOneNode.java
+++ src/main/java/lol/hub/headlessbot/behavior/nodes/composites/FallbackOneNode.java
@@ -3,12 +3,16 @@ package lol.hub.headlessbot.behavior.nodes.composites;
 import lol.hub.headlessbot.behavior.State;
 import lol.hub.headlessbot.behavior.nodes.Node;
 
+import java.util.Objects;
+
 import static lol.hub.headlessbot.behavior.State.*;
 
 /**
  * OR
  * Execute children in order, one per tick,
  * until a child returns SUCCESS.
+ * Returns RUNNING until the end is reached,
+ * then returns FAILURE.
  */
 public final class FallbackOneNode extends CompositeNode {
     private int index = 0;
@@ -19,17 +23,13 @@ public final class FallbackOneNode extends CompositeNode {
 
     @Override
     public synchronized State tick() {
-        switch (children().get(index++).tick()) {
-            case SUCCESS -> {
-                index = 0;
-                return SUCCESS;
-            }
-            case FAILURE -> {
-                if (index >= children().size()) {
-                    index = 0;
-                    return FAILURE;
-                }
-            }
+        if (index >= children().size()) {
+            index = 0;
+            return FAILURE;
+        }
+        if (children().get(index++).tick() == SUCCESS) {
+            index = 0;
+            return SUCCESS;
         }
         return RUNNING;
     }
--- src/main/java/lol/hub/headlessbot/behavior/nodes/composites/SequenceOneNode.java
+++ src/main/java/lol/hub/headlessbot/behavior/nodes/composites/SequenceOneNode.java
@@ -3,12 +3,16 @@ package lol.hub.headlessbot.behavior.nodes.composites;
 import lol.hub.headlessbot.behavior.State;
 import lol.hub.headlessbot.behavior.nodes.Node;
 
+import java.util.Objects;
+
 import static lol.hub.headlessbot.behavior.State.*;
 
 /**
  * AND
  * Execute children in order, one per tick,
  * until a child returns FAILURE.
+ * Returns RUNNING until the end is reached,
+ * then returns SUCCESS.
  */
 public final class SequenceOneNode extends CompositeNode {
     private int index = 0;
@@ -19,17 +23,13 @@ public final class SequenceOneNode extends CompositeNode {
 
     @Override
     public synchronized State tick() {
-        switch (children().get(index++).tick()) {
-            case SUCCESS -> {
-                if (index >= children().size()) {
-                    index = 0;
-                    return SUCCESS;
-                }
-            }
-            case FAILURE -> {
-                index = 0;
-                return FAILURE;
-            }
+        if (index >= children().size()) {
+            index = 0;
+            return SUCCESS;
+        }
+        if (children().get(index++).tick() == FAILURE) {
+            index = 0;
+            return FAILURE;
         }
         return RUNNING;
     }
