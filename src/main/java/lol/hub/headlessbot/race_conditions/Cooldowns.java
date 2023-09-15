73afaa6ab97953a8bbac837fd0a2a59f6b17db44
Florian Hübner <code@hub.lol>
2023-09-15 23:37:45 +0200

    respawn cooldown

--- src/main/java/lol/hub/headlessbot/race_conditions/Cooldowns.java
+++ src/main/java/lol/hub/headlessbot/race_conditions/Cooldowns.java
@@ -4,7 +4,16 @@ import java.time.temporal.ChronoUnit;
 
 public class Cooldowns {
 
-    public static final ExpiringFlag baritone = new ExpiringFlag(3,
-        ChronoUnit.SECONDS);
+    public static final ExpiringFlag baritone = new ExpiringFlag(
+        4,
+        ChronoUnit.SECONDS,
+        false
+    );
+
+    public static final ExpiringFlag respawn = new ExpiringFlag(
+        1,
+        ChronoUnit.SECONDS,
+        false
+    );
 
 }
