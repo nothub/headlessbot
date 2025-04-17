1db3d63a79dd17ab0efb6375af42c286316c5b5d
Florian Hübner <fhuebner@posteo.de>
2025-04-17 17:13:42 +0200

    Update HeadlessMc to 2.5.1

--- scripts/bot.sh
+++ scripts/bot.sh
@@ -4,15 +4,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-hmc() {
-    docker run -it --rm \
-        --name "headlessbot" \
-        -p "127.0.0.1:8080:8080" \
-        -v "${PWD}/mc:/work/.minecraft" \
-        -v "${PWD}/hmc:/work/HeadlessMC" \
-        "n0thub/headlessmc:${hmc_version}" \
-        "${@}"
-}
+project_root="$(dirname "$(realpath "$0")")/.."
+cd "${project_root}"
 
 prop() {
     cat gradle.properties \
@@ -24,15 +17,12 @@ mc_version="$(prop 'minecraft_version')"
 hmc_version="$(prop 'headlessmc_version')"
 fabric_api_version="$(prop 'fabric_api_version')"
 
-# go to project root
-cd "$(dirname "$(realpath "$0")")/.."
-
 mkdir -p run
 cd run
 
 # msa login
 if test ! -f "hmc/auth/.account.json"; then
-    if test ! -f "../auth.json"; then
+    if test ! -f "${project_root}/auth.json"; then
         set +o nounset
         if test -z "${1}" || test -z "${2}"; then
             echo "Usage: ${0} [<username> <password>]" >&2
@@ -40,25 +30,20 @@ if test ! -f "hmc/auth/.account.json"; then
             exit 1
         fi
         set -o nounset
-        jq ".username = \"${1}\" | .password = \"${2}\"" ../auth.example.json > ../auth.json
+        jq ".username = \"${1}\" | .password = \"${2}\"" \
+            "${project_root}/auth.example.json" > "${project_root}/auth.json"
     fi
-    username="$(cat "../auth.json" | jq -r '.username')"
-    password="$(cat "../auth.json" | jq -r '.password')"
-    hmc login "${username}" "${password}"
-fi
-
-# download mc
-if test ! -e "mc/versions/${mc_version}"; then
-    hmc download "${mc_version}"
-fi
-
-# download fabric
-if test -z "$(find './mc/versions/' -maxdepth 1 -name "fabric-loader-*-${mc_version}" -print)"; then
-    hmc fabric "${mc_version}"
+    username="$(cat "${project_root}/auth.json" | jq -r '.username')"
+    password="$(cat "${project_root}/auth.json" | jq -r '.password')"
+    docker run -t --rm \
+        --name "headlessbot" \
+        -v "${PWD}/mc:/work/.minecraft" \
+        -v "${PWD}/hmc:/work/HeadlessMC" \
+        "n0thub/headlessmc:${hmc_version}" \
+        login "${username}" "${password}"
 fi
 
 dlmod() {
-    mkdir -p "mc/mods"
     curl -fsSL -o "${1}" "${2}"
     if ! file --mime-type "${1}" | grep -E '.+: application/java-archive' 1> /dev/null; then
         printf >&2 '%s is not a jar file!\n' "${1}"
@@ -68,6 +53,8 @@ dlmod() {
     fi
 }
 
+mkdir -p "mc/mods"
+
 dlmod 'mc/mods/fabric-api.jar' \
     "https://github.com/FabricMC/fabric/releases/download/${fabric_api_version}/fabric-api-${fabric_api_version}.jar"
 
@@ -75,15 +62,17 @@ dlmod 'mc/mods/baritone-api-fabric.jar' \
     "https://github.com/nothub/baritone-mirror/raw/refs/heads/main/${mc_version}/baritone-api-fabric-${mc_version}.jar"
 
 (
-    # go to project root
-    cd ..
-
-    # build our mod
+    # build bot
+    cd "${project_root}"
     ./gradlew --console=plain --info --full-stacktrace clean check build
-
-    # install our mod
     cp ./build/libs/headlessbot.jar run/mc/mods/
 )
 
 # launch bot
-hmc "launch 1 -id"
+docker run -it --rm \
+    --name "headlessbot" \
+    -p "127.0.0.1:8080:8080" \
+    -v "${PWD}/mc:/work/.minecraft" \
+    -v "${PWD}/hmc:/work/HeadlessMC" \
+    "n0thub/headlessmc:${hmc_version}" \
+    launch "fabric:${mc_version}"
